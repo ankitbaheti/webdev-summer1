ff5d26f81f729e5174ef6ab29699959b318c776f
ankitbaheti <ankit.baheti04@gmail.com>
2018-05-12 11:32:32 -0400

    created user.model.client.js

---
+++ src/main/webapp/jquery/models/user.model.client.js
@@ -0,0 +1,104 @@
+function User(username, password, firstName, lastName, id, phone, email, role, dateOfBirth) {
+    this.username = username;
+    this.password = password;
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.id = id;
+    this.phone = phone;
+    this.email = email;
+    this.role = role;
+    this.dateOfBirth = dateOfBirth;
+
+
+
+    this.setUsername = setUsername;
+    this.getUsername = getUsername;
+    this.setPassword = setPassword;
+    this.getPassword = getPassword;
+    this.setFirstName = setFirstName;
+    this.getFirstName = getFirstName;
+    this.setLastName = setLastName;
+    this.getLastName = getLastName;
+    this.setId = setId;
+    this.getId = getId;
+    this.setPhone = setPhone;
+    this.getPhone = getPhone;
+    this.setEmail = setEmail;
+    this.getEmail = getEmail;
+    this.setRole = setRole;
+    this.getRole = getRole;
+    this.setDateOfBirth = setDateOfBirth;
+    this.getDateOfBirth = getDateOfBirth;
+
+    function setDateOfBirth(dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    function getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    function setRole(role) {
+        this.role = role;
+    }
+
+    function getRole() {
+        return this.role;
+    }
+
+    function setEmail(email) {
+        this.email = email;
+    }
+
+    function getEmail() {
+        return this.email;
+    }
+
+    function setPhone(phone) {
+        this.phone = phone;
+    }
+
+    function getPhone() {
+        return this.phone;
+    }
+
+    function setId(id) {
+        this.id = id;
+    }
+
+    function getId() {
+        return this.id;
+    }
+
+    function setLastName(lastName) {
+        this.lastName = lastName;
+    }
+
+    function getLastName() {
+        return this.lastName;
+    }
+
+    function setFirstName(firstName) {
+        this.firstName = firstName;
+    }
+
+    function getFirstName() {
+        return this.firstName;
+    }
+
+    function setPassword(password) {
+        this.password = password;
+    }
+
+    function getPassword() {
+        return this.password;
+    }
+
+    function setUsername(username) {
+        this.username = username;
+    }
+    function getUsername() {
+        return this.username;
+    }
+    // ...same for rest of properties…
+}
