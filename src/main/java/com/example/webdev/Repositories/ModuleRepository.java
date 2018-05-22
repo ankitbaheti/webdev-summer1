bd1e00daf632b4bd36717965a42ed1e7e7a64f3b
ankitbaheti <ankit.baheti04@gmail.com>
2018-05-22 14:37:27 -0400

    created module model, repository and service class

---
+++ src/main/java/com/example/webdev/Repositories/ModuleRepository.java
@@ -0,0 +1,7 @@
+package com.example.webdev.Repositories;
+
+import com.example.webdev.Models.Module;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ModuleRepository extends CrudRepository<Module, Integer> {
+}
---
+++ src/main/java/com/example/webdev/Services/ModuleService.java
@@ -0,0 +1,51 @@
+package com.example.webdev.Services;
+
+import com.example.webdev.Models.Course;
+import com.example.webdev.Models.Module;
+import com.example.webdev.Repositories.CourseRepository;
+import com.example.webdev.Repositories.ModuleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
+public class ModuleService {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @Autowired
+    ModuleRepository moduleRepository;
+
+    @PostMapping("/api/course/{courseId}/module")
+    Module createModule(@PathVariable("courseId") int courseId,
+                        @RequestBody Module module){
+        if(courseRepository.findById(courseId).isPresent()){
+            Course course = courseRepository.findById(courseId).get();
+            module.setCourse(course);
+            return moduleRepository.save(module);
+        }
+        return null;
+    }
+
+    @GetMapping("/api/course/{courseId}/module")
+    List<Module> findAllModuleForCourse(@PathVariable("courseId") int courseId){
+        if(courseRepository.findById(courseId).isPresent()){
+            Course course = courseRepository.findById(courseId).get();
+            return course.getModules();
+        }
+        return null;
+    }
+
+    @GetMapping("/api/module")
+    List<Module>  findAllModule(){
+        return (List<Module>) moduleRepository.findAll();
+    }
+
+    @DeleteMapping("/api/module/{moduleId}")
+    void deleteModule(@PathVariable("moduleId") int moduleId){
+        moduleRepository.deleteById(moduleId);
+    }
+}
