ae177ddbb3d472ef8a3f1e393eba8b0ea95a0c48
ankitbaheti <ankit.baheti04@gmail.com>
2018-05-29 09:50:54 -0400

    made changes to lesson model class

--- src/main/java/com/example/webdev/Models/Lesson.java
+++ src/main/java/com/example/webdev/Models/Lesson.java
@@ -3,6 +3,8 @@ package com.example.webdev.Models;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Lesson {
@@ -14,6 +16,12 @@ public class Lesson {
     @ManyToOne
     @JsonIgnore
     private Module module;
+
+    @OneToMany(mappedBy = "lesson")
+    @OrderBy("orderNumber ASC")
+    @JsonIgnore
+    private List<Widget> widgets;
+
 
     public Lesson() {
     }
@@ -40,6 +48,14 @@ public class Lesson {
 
     public void setModule(Module module) {
         this.module = module;
+    }
+
+    public List<Widget> getWidgets() {
+        return widgets;
+    }
+
+    public void setWidgets(List<Widget> widgets) {
+        this.widgets = widgets;
     }
 
     public void updateLesson(Lesson lesson){
